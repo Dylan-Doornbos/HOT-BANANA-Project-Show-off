e77c7ba8edcc062fad786034b4e6602a29fe7c0a
Dylan <482324@student.saxion.nl>
2022-05-25 11:05:37 +0200

    Fixed abrupt starting corner for the path movement.

--- Assets/_Project/Scripts/Movement/PathMovement.cs
+++ Assets/_Project/Scripts/Movement/PathMovement.cs
@@ -1,45 +1,71 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using DG.Tweening;
-using DG.Tweening.Core;
-using DG.Tweening.Plugins.Options;
 using UnityEngine;
 
 public class PathMovement : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private Path _path;
-    [SerializeField] private float _moveSpeed;
 
-    private TweenerCore<Vector3, DG.Tweening.Plugins.Core.PathCore.Path, PathOptions> tweener;
+    [Header("Properties")]
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private bool _rotateUpDown;
 
     private Vector3 _lastPosition;
+    private List<Vector3> _waypoints;
+
+    Tween _moveTween;
 
     private void Start()
     {
-        transform.position = _path.waypoints[0];
-        run();
+        _waypoints = getWaypoints();
+        followPath();
     }
 
     private void Update()
     {
         if (transform.position == _lastPosition) return;
-        
-        transform.forward = transform.position - _lastPosition;
-        
+
+        Vector3 forward = transform.position - _lastPosition;
+        forward.y = _rotateUpDown ? forward.y : 0;
+        transform.forward = forward;
+
         _lastPosition = transform.position;
     }
 
-    private void run()
+    private List<Vector3> getWaypoints()
+    {
+        int listSize = _path.waypoints.Length;
+
+        List<Vector3> waypoints = new List<Vector3>();
+        waypoints.Add(_path.waypoints[listSize - 2]);
+        waypoints.Add(_path.waypoints[listSize - 1]);
+        waypoints.AddRange(_path.waypoints.ToList());
+        waypoints.Add(_path.waypoints[0]);
+        waypoints.Add(_path.waypoints[1]);
+
+        return waypoints;
+    }
+
+    private void followPath()
     {
         float loopDuration = _path.GetDistance() / _moveSpeed;
 
-        List<Vector3> waypoints = _path.waypoints.ToList();
-        waypoints.Add(waypoints[0]);
-        tweener?.Kill();
-        tweener = transform.DOPath(waypoints.ToArray(), loopDuration, PathType.CatmullRom, PathMode.Full3D, 10,
-            Color.white);
-        tweener.SetLoops(-1, LoopType.Restart);
-        tweener.SetEase(Ease.Linear);
+        _moveTween?.Kill();
+
+        _moveTween = transform.DOPath(_waypoints.ToArray(), loopDuration, PathType.CatmullRom, PathMode.Full3D, 10,
+            new Color(0, 0, 0, 0))
+            .SetEase(Ease.Linear).OnWaypointChange(onWaypointChanged);
+
+        _moveTween.GotoWaypoint(2, true);
+    }
+
+    private void onWaypointChanged(int index)
+    {
+        if(index == _path.waypoints.Length + 2)
+        {
+            followPath();
+        }
     }
 }
