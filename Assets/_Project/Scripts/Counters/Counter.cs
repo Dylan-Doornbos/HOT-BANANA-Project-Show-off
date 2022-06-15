fca5a5a9a2efd01f2593e51420d4899f277638cb
Dylan <482324@student.saxion.nl>
2022-06-15 11:52:53 +0200

    Auto stash before merge of "Versions/Version-0.1" and "origin/Versions/Version-0.1"

--- Assets/_Project/Scripts/Counters/Counter.cs
+++ Assets/_Project/Scripts/Counters/Counter.cs
@@ -1,24 +1,44 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Counter : MonoBehaviour
 {
+    [SerializeField] private bool _startActive = true;
     public UnityEvent onCounterIncreased;
+    
+    private bool _isCounting;
+
+    private void Awake()
+    {
+        _isCounting = _startActive;
+    }
 
     public int count { get; protected set; }
 
     public void IncreaseCounter(int pAmount)
     {
-        setCount(count + pAmount);
+        if (_isCounting) setCount(count + pAmount);
     }
 
     public void Reset()
     {
         setCount(0);
     }
+
     private void setCount(int pCount)
     {
         count = pCount;
         onCounterIncreased?.Invoke();
+    }
+
+    public void ContinueCounting()
+    {
+        _isCounting = true;
+    }
+
+    public void StopCounting()
+    {
+        _isCounting = false;
     }
 }
